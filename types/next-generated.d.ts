eafcd61399f0eae604875aaa9ebc0427e460e393
Danylo Kalynovskyi <112323861+electromalina@users.noreply.github.com>
2025-11-10 15:40:42 +0100

    vercel path fix

--- types/next-generated.d.ts
+++ types/next-generated.d.ts
@@ -1,10 +1,10 @@
 declare module "../../app/page.js" {
-  export * from "@/app/page";
-  export { default } from "@/app/page";
+  export * from "../../app/page";
+  export { default } from "../../app/page";
 }
 
 declare module "../../app/layout.js" {
-  export * from "@/app/layout";
-  export { default } from "@/app/layout";
+  export * from "../../app/layout";
+  export { default } from "../../app/layout";
 }
 
