8b08766f99fe9bf9598703719f75f91ceb96fa1b
Danylo Kalynovskyi <112323861+electromalina@users.noreply.github.com>
2025-11-10 15:29:36 +0100

    bug fix

---
+++ types/next-generated.d.ts
@@ -0,0 +1,10 @@
+declare module "../../app/page.js" {
+  export * from "@/app/page";
+  export { default } from "@/app/page";
+}
+
+declare module "../../app/layout.js" {
+  export * from "@/app/layout";
+  export { default } from "@/app/layout";
+}
+
